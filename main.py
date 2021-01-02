a55975e13c140b1dc6f9249597ece911fb192f0c
Conary36 <crossingwaters@icloud.com>
2021-01-01 21:00:14 -0500

    ..saving...

--- main.py
+++ main.py
@@ -12,8 +12,25 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 
 train_images = train_images/255.0 # truncate pixel color scale to 1 (255/255)
 test_images = test_images/255.0 # truncate pixel color scale to 1 (255/255)
+# print(train_images[7]) # print in console the structured layout of data
 
-print(train_images[7]) # print in console the structured layout of data
+# plt.imshow(train_images[7], cmap=plt.cm.binary)
+# plt.show()
+"""
+NOW LETS BUILD NEURAL NETWORK MODEL
+"""
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
 
-plt.imshow(train_images[7], cmap=plt.cm.binary)
-plt.show()
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("Test Accuracy", test_acc)
+
+
+
