49b2dfb0dc29c21a6acf18a5a600484479e7d722
Conary36 <crossingwaters@icloud.com>
2021-01-01 20:01:14 -0500

    structued data scale to 1

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+data = keras.datasets.fashion_mnist  # pull in data from google
 
-data = keras.datasets.fashion_mnist
-
-(train_images, train_labels ), (test_images, test_labels) = data.load_data()
+(train_images, train_labels ), (test_images, test_labels) = data.load_data() # structure data
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images/255.0 # truncate pixel color scale to 1 (255/255)
+test_images = test_images/255.0 # truncate pixel color scale to 1 (255/255)
+
+print(train_images[7]) # print in console the structured layout of data
 
 plt.imshow(train_images[7], cmap=plt.cm.binary)
 plt.show()
